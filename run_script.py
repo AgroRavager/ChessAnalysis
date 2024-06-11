c00e2a43712932257d193026fd87d3514ed43609
AgroRavager <62581399+AgroRavager@users.noreply.github.com>
2024-06-11 09:05:05 -0700

    Add files via upload

--- run_script.py
+++ run_script.py
@@ -1,18 +1,30 @@
+# Shaurya Jeloka, Ian Jeong, Akshay Vakharia
+# 6/9/2024
+
+# This program is a script that runs the update_prices() function defined in price_tracker_functions.py
+# every ten minutes to get the prices of all items being tracked from the internet and update
+# the databse. This program is meant to run continuously in the background to collect data 
+# at a frequent yet reasonable rate.
+
 import schedule
 import time
 import price_tracker_functions as functions
 
-def job():
-    print("Task is running")
-
-# Schedule the task to run every 30 minutes
+# Schedule the update_prices function to run every ten minutes
 schedule.every(10).minutes.do(functions.update_prices)
 
-# Function to run the schedule loop
+# Define the function to run the scheduled loop
 def run_schedule():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)  # Sleep for a short time to avoid busy-waiting
 
+    #run a continuous loop
+    while True:
+        
+        # Check and run pending tasks
+        schedule.run_pending() 
+
+        #briefly pause the loop to avoid busy waiting
+        time.sleep(1)
+
+#execute run_schedule function
 if __name__ == "__main__":
     run_schedule()
